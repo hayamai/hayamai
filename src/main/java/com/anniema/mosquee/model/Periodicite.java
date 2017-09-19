d913df52a965c7421c18dda7a4970277fc0267e3
aboukhadija <aboukhadija.rachid@gmail.com>
2017-09-19 19:28:09 +0200

    install

---
+++ src/main/java/com/anniema/mosquee/model/Periodicite.java
@@ -0,0 +1,12 @@
+package com.anniema.mosquee.model;
+
+import java.io.Serializable;
+
+public class Periodicite implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8089822908551248294L;
+
+}
---
+++ src/main/java/com/anniema/mosquee/model/TypeEvenement.java
@@ -0,0 +1,12 @@
+package com.anniema.mosquee.model;
+
+import java.io.Serializable;
+
+public class TypeEvenement implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 861511299407490345L;
+
+}
